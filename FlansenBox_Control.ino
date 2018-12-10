80e3375d568687f64849e8903c77444ca88fb8b4
Leo Preissegger <6155@htl.rennweg.at>
2018-12-10 10:30:17 +0100

    rewrote watering procedure

--- FlansenBox_Control.ino
+++ FlansenBox_Control.ino
@@ -6,40 +6,38 @@ void setup() {
 
 void loop() {
   controlFAN(false);
-  if(millis() - lastTimeFans > 1000 * 60 * 60 * 3) { //run the fans every 3 hours for 10 min (line 15)
+  //run the fans for the sleep time defined on line 15
+  if(millis() - lastTimeFans > 1000 * 60 * 60 * 3) { //run the fans every 3 hours for 10 min
     controlFAN(true);
     lastTimeFans = millis();
   }
   checkMoistureAndWater();
-  checkWaterInTank();
   delay(1000 * 60 * 10); //wait 10 min
 }
 
 
 void checkMoistureAndWater() { //TODO: test moisture values
-  if(getMoisture1() < 500) {
-    waterUpperStage();
-  }
-  if(getMoisture2() < 500) {
-    waterLowerStage();
-  }
-}
+  bool watering1 = getMoisture1() < 500;
+  bool watering2 = getMoisture2() < 500;
+  
+  unsigned long wateringStarted = millis();
+  
+  while(watering1 || watering2) {
+    if(!checkWaterInTank()) {
+      watering1 = false;
+      watering2 = false;
+    }
+    
+    if(watering1) {
+      watering1 = millis() - wateringStarted < 1000 * 5; 
+    }
+    controlPump1(watering1);
 
-void waterUpperStage() {
-  if(checkWaterInTank()) {
-    controlPump1(true);
-    delay(1000 * 10); //TODO: test watering times
-    controlPump1(false);
-    checkWaterInTank();
-  }
-}
-
-void waterLowerStage() {
-  if(checkWaterInTank()) {
-    controlPump2(true);
-    delay(1000 * 10); //TODO: test watering times
-    controlPump2(false);
-    checkWaterInTank();
+    if(watering2) {
+      watering2 = millis() - wateringStarted < 1000 * 5; 
+    }
+    controlPump2(watering2);
+    delay(1);
   }
 }
 
