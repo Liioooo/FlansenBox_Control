d8cbce4d80311e89095d5e23263b417578b99c58
Liiioooo <preissegger.leo@a1.net>
2018-12-16 11:57:38 +0100

    added PWM for fans and pumps

--- FlansenBox_Control.ino
+++ FlansenBox_Control.ino
@@ -1,6 +1,7 @@
 #define FAN_INTERVAL 1000 * 60 * 60 * 3
 #define LOOP_INTERVAL 1000 * 60 * 10
-#define PUMP_DURATION 1000 * 5
+#define PUMP1_DURATION 1000 * 5
+#define PUMP2_DURATION 1000 * 5
 
 unsigned long lastTimeFans = 0;
 
@@ -10,7 +11,7 @@ void setup() {
 
 void loop() {
   controlFAN(false);
-  //run the fans for the sleep time defined on line 15
+  //run the fans for the sleep time defined on line 19
   if(millis() - lastTimeFans > FAN_INTERVAL) { //run the fans every 3 hours for 10 min
     controlFAN(true);
     lastTimeFans = millis();
@@ -20,28 +21,41 @@ void loop() {
 }
 
 
-void checkMoistureAndWater() { //TODO: test moisture values
+void checkMoistureAndWater() { 
+  //TODO: test moisture values
   bool watering1 = getMoisture1() < 500;
   bool watering2 = getMoisture2() < 500;
   
   unsigned long wateringStarted = millis();
-  
+
+  if(!checkWaterInTank()) {
+      watering1 = false;
+      watering2 = false;
+  }
+
+  controlPump1(watering1);
+  controlPump2(watering2);
+
   while(watering1 || watering2) {
     if(!checkWaterInTank()) {
       watering1 = false;
       watering2 = false;
     }
-    
-    if(watering1) {
-      watering1 = millis() - wateringStarted < PUMP_DURATION;
-    }
-    controlPump1(watering1);
 
-    if(watering2) {
-      watering2 = millis() - wateringStarted < PUMP_DURATION;
+    if(millis() - wateringStarted > PUMP1_DURATION) {
+      watering1 = false;
     }
-    controlPump2(watering2);
-    delay(1);
+    if(millis() - wateringStarted > PUMP2_DURATION) {
+      watering2 = false;
+    }
+    
+    if(!watering1) {
+      controlPump1(false);
+    }
+    if(!watering2) {
+      controlPump2(false);
+    }
+    
   }
 }
 
--- functions.ino
+++ functions.ino
@@ -1,41 +1,64 @@
-#define pump1Pin 13
-#define pump2Pin 12
+#define pump1Pin 9
+#define pump2Pin 10
 #define fanPin 11
-#define LEDStripPin 10
-#define waterLEDPin 9
+#define LEDStripPin 8
+
+#define waterLEDPin 7 //red LED, water indicator
+
 #define soilSensor1Pin A0
 #define soilSensor2Pin A1
-#define waterInTankPin A2
+#define soilSensor1Current 5 // current for sensor 1
+#define soilSensor2Current 6 // current for sensor 2
+
+#define waterInTankPin A2 //input pin for water measering
+#define waterInTankCurrent 4 // current for water tank measering
 
 void setPinModes() {
+  // set timers for PWM frequency otside the hearing spektrum 
+  TCCR1B = TCCR1B & 0b11111000 | 0x01; // pin 9,10
+  TCCR2B = TCCR2B & 0b11111000 | 0x01; // pin 11,3
+  
   pinMode(pump1Pin, OUTPUT);
   pinMode(pump2Pin, OUTPUT);
   pinMode(fanPin, OUTPUT);
   pinMode(LEDStripPin, OUTPUT);
   pinMode(waterLEDPin, OUTPUT);
+  pinMode(soilSensor1Current, OUTPUT);
+  pinMode(soilSensor2Current, OUTPUT);
+  pinMode(waterInTankCurrent, OUTPUT);
+
 }
 
 void controlPump1(bool state) {
   if(state) {
-    digitalWrite(pump1Pin, HIGH);
+    // drossels the speed dow
+    analogWrite(pump1Pin, 255);
+    delay(800); // start the motor
+    analogWrite(pump1Pin, 150);
   } else {
-    digitalWrite(pump1Pin, LOW);
+    analogWrite(pump1Pin, 0);
   }
 }
 
 void controlPump2(bool state) {
   if(state) {
-    digitalWrite(pump2Pin, HIGH);
+    // drossels the speed dow
+    analogWrite(pump2Pin, 255);
+    delay(800); // start the motor
+    analogWrite(pump2Pin, 150);
   } else {
-    digitalWrite(pump2Pin, LOW);
+    analogWrite(pump2Pin, 0);
   }
 }
 
 void controlFAN(bool state) {
   if(state) {
-    digitalWrite(fanPin, HIGH);
+    // drossels the speed dow
+    analogWrite(fanPin, 255);
+    delay(800); // start the motor
+    analogWrite(fanPin, 150);
   } else {
-    digitalWrite(fanPin, LOW);
+    analogWrite(fanPin, 0);
   }
 }
 
@@ -56,14 +79,25 @@ void controlWaterLED(bool state) {
 }
 
 bool isWaterInTank() {
+  digitalWrite(waterInTankCurrent, HIGH);
+  delay(200);
   int value = analogRead(waterInTankPin);
+  digitalWrite(waterInTankCurrent, LOW);
   return value > 800; //TODO: test the value
 }
 
 int getMoisture1() {
-  return analogRead(soilSensor1Pin);
+  digitalWrite(soilSensor1Current, HIGH);
+  delay(400);
+  int val =  analogRead(soilSensor1Pin);
+  digitalWrite(soilSensor2Current, LOW);
+  return val;
 }
 
 int getMoisture2() {
-  return analogRead(soilSensor2Pin);
+  digitalWrite(soilSensor2Current, HIGH);
+  delay(400);
+  int val = analogRead(soilSensor2Pin);
+  digitalWrite(soilSensor2Current, LOW);
+  return val;
 }
