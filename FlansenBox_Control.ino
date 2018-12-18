e72aebf4afdc308157de932aed90d4b2ed9c6a33
Leo Preissegger <6155@htl.rennweg.at>
2018-12-18 17:34:50 +0100

    tested values, test values now

--- FlansenBox_Control.ino
+++ FlansenBox_Control.ino
@@ -1,8 +1,12 @@
-const unsigned long FAN_INTERVAL = 1000l*60*60*3;
-const unsigned long LOOP_INTERVAL = 1000l*60*10;
+=============
+TEST VALUE
+=============
 
-const unsigned long PUMP_DURATION_UPPER = 1000*5;
-const unsigned long PUMP_DURATION_LOWER = 1000*5;
+const unsigned long FAN_INTERVAL = 1000l*12; //12 sek
+const unsigned long LOOP_INTERVAL = 1000l*10; //10 sek
+
+const unsigned long PUMP_DURATION_UPPER = 1000*2;
+const unsigned long PUMP_DURATION_LOWER = 1000*2;
 
 //TODO: test moisture values
 #define MIN_MOISTURE_UPPER 500
@@ -14,15 +18,25 @@ void setup() {
   setPinModes();
 }
 
+bool isRunning = 1==0;
+bool dauerPump = 1==0;
+
 void loop() {
-  controlFan(false);
-  //run the fans for the sleep time defined on line 19
-  if(millis() - lastTimeFans > FAN_INTERVAL) { //run the fans every 3 hours for 10 min
-    controlFan(true);
-    lastTimeFans = millis();
+  if (isRunning) {
+    controlFan(false);
+    controlLEDStrip(false);
+    //run the fans for the sleep time defined on line 19
+    if(millis() - lastTimeFans > FAN_INTERVAL) { //run the fans every 3 hours for 10 min
+      controlFan(true);
+      controlLEDStrip(true);
+      lastTimeFans = millis();
+    }
+    checkMoistureAndWater();
+    delay(LOOP_INTERVAL); //wait 10 min
+  } else if (dauerPump) {
+    controlPumpUpper(true);
+    controlPumpLower(true);
   }
-  checkMoistureAndWater();
-  delay(LOOP_INTERVAL); //wait 10 min
 }
 
 
